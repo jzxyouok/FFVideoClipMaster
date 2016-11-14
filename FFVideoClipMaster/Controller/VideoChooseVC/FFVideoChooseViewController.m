44843c96600c3b8e89ab84b6ecacf7ff60b3367d
Mr.Yao <yaoquanfeng@goumin.com>
2016-11-14 22:33:44 +0800

    add text

--- FFVideoClipMaster/Controller/VideoChooseVC/FFVideoChooseViewController.m
+++ FFVideoClipMaster/Controller/VideoChooseVC/FFVideoChooseViewController.m
@@ -10,8 +10,16 @@
 #import "LSYNavigationController.h"
 #import "LSYAlbumCatalog.h"
 
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+#define SystemVersion [[UIDevice currentDevice].systemVersion floatValue]
+
+
 @interface FFVideoChooseViewController ()<LSYAlbumCatalogDelegate>
 
+@property (nonatomic, strong) NSURL * url;
 @end
 
 @implementation FFVideoChooseViewController
@@ -30,10 +38,40 @@
         }
         else if ([[asset valueForProperty:@"ALAssetPropertyType"] isEqual:@"ALAssetTypeVideo"]){
             NSURL *url = asset.defaultRepresentation.url;
-            NSLog(@"%@",url);
+            self.url = url;
         }
     }
 }
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    if (self.url) {
+        [self vipPlayerViewViewControllerWithVideoUrl:self.url];
+        self.url = nil;
+    }
+}
+
+
+- (void)vipPlayerViewViewControllerWithVideoUrl:(NSURL *)urlString {
+    if (SystemVersion >= 9.0) {
+        AVPlayerViewController *playerVC = [[AVPlayerViewController alloc] init];
+        AVPlayerItem *item = [AVPlayerItem playerItemWithURL:urlString];
+        playerVC.player = [AVPlayer playerWithPlayerItem:item];
+        [self presentViewController:playerVC
+                                                               animated:YES
+                                                             completion:^{
+                                                                 [playerVC.player play];
+                                                             }];
+    } else {
+        MPMoviePlayerViewController *moviePlayerVC = [[MPMoviePlayerViewController alloc] initWithContentURL:urlString];
+        [self presentViewController:moviePlayerVC
+                                                               animated:YES
+                                                             completion:^{
+                                                                 [moviePlayerVC.moviePlayer play];
+                                                             }];
+    }
+}
+
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     LSYAlbumCatalog *albumCatalog = [[LSYAlbumCatalog alloc] init];
