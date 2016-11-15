5f1cc13b54e9fc62656a3fa500321a48d23e656e
李姝睿 <1352736967@qq.com>
2016-11-15 14:43:48 +0800

    no message

--- FFVideoClipMaster/AppDelegate.m
+++ FFVideoClipMaster/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    
     self.window.backgroundColor = [UIColor clearColor];
     FFVideoChooseViewController *chooseVC = [[FFVideoChooseViewController alloc]init];
     UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:chooseVC];
